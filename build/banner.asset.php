b5421f4f34c60e0d358529c18e2a8ac6ba211839
Haly Yampolska <halyna.yampolska@gmail.com>
2023-11-24 21:04:54 +0200

    Finished Banner Block With Background Image

--- build/banner.asset.php
+++ build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks'), 'version' => 'bb807f9ae62c358c337f');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0cc46a7d3a2cd15821f4');
--- functions.php
+++ functions.php
@@ -184,8 +184,9 @@ function makeNotePrivate($data, $postarr) {
 // Register All Types Of Blocks
 
   class JSXBlock {
-    function __construct($name, $renderCallback = null) {
+    function __construct($name, $renderCallback = null, $data = null) {
         $this->name = $name;
+        $this->data = $data;
         $this->renderCallback = $renderCallback;
         add_action('init', [$this, 'onInit']);
     }
@@ -199,6 +200,10 @@ function makeNotePrivate($data, $postarr) {
     function onInit() {
         wp_register_script($this->name, get_stylesheet_directory_uri() . "/build/{$this->name}.js", array('wp-blocks', 'wp-editor'));
         
+        if($this->data) {
+            wp_localize_script($this->name, $this->name, $this->data);
+        }
+
         $ourArgs = array(
             'editor_script' => $this->name
         );
@@ -212,6 +217,6 @@ function makeNotePrivate($data, $postarr) {
   }
 
   // Create a Instanses For Block Types
-  new JSXBlock('banner', true);
+  new JSXBlock('banner', true, ['fallbackimage' => get_theme_file_uri('/images/library-hero.jpg')]);
   new JSXBlock('genericheading');
   new JSXBlock('genericbutton');
