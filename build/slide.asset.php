6a291e310b614257c6fbee0dfa49fc282e160e4a
Haly Yampolska <halyna.yampolska@gmail.com>
2023-11-26 23:46:59 +0200

    Created Slideshow Block (part 1)

---
+++ build/slide.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '188276caa60ffd5fd62c');
--- functions.php
+++ functions.php
@@ -247,3 +247,5 @@ class PlaceholderBlock {
   new JSXBlock('banner', true, ['fallbackimage' => get_theme_file_uri('/images/library-hero.jpg')]);
   new JSXBlock('genericheading');
   new JSXBlock('genericbutton');
+  new JSXBlock('slideshow', true);
+  new JSXBlock('slide', true);
